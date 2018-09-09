e0d172d1d2e41ed8495bb702be757d3aa2291799
flysaiah <mayeis01@yahoo.com>
2018-09-08 23:05:08 -0400

    Styling, fonts & colors

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import ChallengeInterface from './containers/ChallengeInterface/ChallengeInterface';
 import Sidebar from './components/Sidebar/Sidebar';
 import './App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import lightBlue from '@material-ui/core/colors/lightBlue';
 
 class App extends Component {
+
   render () {
+    const theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#00b0ff'
+        }
+      },
+    });
     return (
-      <div className="App">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
-        <Sidebar />
-        <ChallengeInterface />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+
+          <Sidebar />
+          <ChallengeInterface />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
